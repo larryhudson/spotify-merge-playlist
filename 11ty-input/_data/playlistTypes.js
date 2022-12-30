a6457039ff659580faae1a510b79fe1760881648
Larry Hudson <larryhudson@hey.com>
2022-12-31 09:22:45 +1100

    tweaking progressive enhancement

---
+++ 11ty-input/_data/playlistTypes.js
@@ -0,0 +1,16 @@
+module.exports = [
+  { name: "Sound", description: "Songs new and old that define the genre" },
+  { name: "Intro", description: "An attempt at an introduction to the genre" },
+  { name: "Pulse", description: "What fans of the genre are listening to now" },
+  {
+    name: "Edge",
+    description:
+      "Recently released music that fans of the genre are checking out",
+  },
+  {
+    name: "♀Filter",
+    description:
+      "An attempt to find music in the genre made wholly or partly by women",
+  },
+  { name: "2022", description: "Music from the genre that came out in 2022" },
+];
