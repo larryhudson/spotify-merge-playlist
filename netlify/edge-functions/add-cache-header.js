5fe85f8d8ad14a4bcd3cb178bfdb1beda896b3ef
Larry Hudson <larryhudson@hey.com>
2023-01-02 08:54:49 +1100

    add caching for edge function - top genres page

---
+++ netlify/edge-functions/add-cache-header.js
@@ -0,0 +1,36 @@
+export default async (request, context) => {
+  const url = new URL(request.url);
+
+  // we only want the URL after the 'origin', eg. /app/top-genres/
+  const pathWithSearch = url.pathname + url.search;
+
+  // helpers for calculating number of seconds
+  const oneDay = 60 * 60 * 24;
+  const oneWeek = oneDay * 7;
+  const twoWeeks = oneDay * 14;
+
+  const cacheDurationByPath = {
+    "/app/top-genres/?timeRange=short_term": oneDay,
+    "/app/top-genres/?timeRange=medium_term": oneWeek,
+    "/app/top-genres/": oneWeek, // same as medium term
+    "/app/top-genres/?timeRange=long_term": twoWeeks,
+  };
+
+  const ageForThisUrl = cacheDurationByPath[pathWithSearch];
+
+  if (ageForThisUrl) {
+    // add the cache header
+    console.log(
+      "adding the cache header for url",
+      pathWithSearch,
+      "max age",
+      ageForThisUrl
+    );
+    const response = await context.next();
+    response.headers.set("Cache-Control", `max-age=${ageForThisUrl}`);
+    return response;
+  } else {
+    // don't add the cache age
+    return context.next();
+  }
+};
